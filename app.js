ef058d0b57ee9761456c4ce8d94b483511378d47
harel zharia <harel.zharia@gmail.com>
2022-09-23 10:14:57 +0300

    adding initial project files

---
+++ app.js
@@ -0,0 +1,43 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+const generateTemplate = (todo) => {
+    const html = `
+            <li class="list-group-item d-flex justify-content-between align-item-center">
+                <span>${todo}</span>
+                <i class="fa-solid fa-circle-minus delete"></i>
+            </li>
+    `;
+    list.innerHTML += html;
+};
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const todo = addForm.add.value.toLowerCase().trim();
+    if(todo.length){
+        generateTemplate(todo);
+        addForm.reset();
+    }
+});
+
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+const filteredToDos = (term) => {
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(term))
+            .forEach(todo => todo.classList.add('filtered'));
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(term))
+        .forEach(todo => todo.classList.remove('filtered'));
+};
+
+search.addEventListener('keyup', e => {
+    const term = search.value.trim().toLowerCase();
+    filteredToDos(term);
+});
